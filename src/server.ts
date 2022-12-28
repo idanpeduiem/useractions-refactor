3bf12aea5a61660102aab277bf747ac5515f2a19
idanpeduiem <62710926+idanpeduiem@users.noreply.github.com>
2022-12-28 11:32:49 +0200

    change apollo to yoga

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,20 @@
-import { ApolloServer } from "@apollo/server";
-import express from "express";
-import http from "http";
 import { getSchema } from "./graphql/schema/schema";
-import {ApolloServerPluginDrainHttpServer} from "@apollo/server/plugin/drainHttpServer";
-import { expressMiddleware } from '@apollo/server/express4';
-import cors from 'cors';
-import {json} from 'body-parser';
-import settings from "./config/settings";
+import { createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
 import logger from "./logger/logger";
 
 (async () => {
+  const yoga = createYoga({
+    schema: getSchema(),
+    context: async ({request: req, params}) => {
+      logger.info(`Got UserAction: ${params.operationName}`);
+      return {};
+    }
+  })
 
-  const app = express();
-  const httpServer = http.createServer(app);
-  const schema = await getSchema();
+  const server = createServer(yoga);
 
-  const server = new ApolloServer<{}>({
-    schema,
-    plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
-  });
-
-  await server.start();
-
-  app.use(
-      cors(),
-      json(),
-      expressMiddleware(server, {
-        context: async ({req}) => {
-          logger.info(`Got UserAction: ${req.body.operationName}`);
-          logger.info(`req: ${req}`);
-          return {};
-        }
-      }),
-  );
-
-  await new Promise((resolve) => {
-    resolve(httpServer.listen({ port: settings.server.port }))
-  });
-
-  console.log(`🚀 Server ready at http://localhost:4000`);
-})();
-
-
+  await server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
+  })
+})()
